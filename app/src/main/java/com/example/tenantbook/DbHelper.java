49c5a66b0a5e6acf063e99e517fa7d16d2add995
Rahul9315 <baghel931515@gmail.com>
2023-11-19 07:25:59 +0000

    updated database and property_activity_2

--- app/src/main/java/com/example/tenantbook/DbHelper.java
+++ app/src/main/java/com/example/tenantbook/DbHelper.java
@@ -1,5 +1,9 @@
 package com.example.tenantbook;
+
+
+import android.content.ContentValues;
 import android.content.Context;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 
@@ -8,13 +12,18 @@ public class DbHelper extends SQLiteOpenHelper {
     private static final int DATABASE_VERSION = 1;
 
 
-    // table names and column names here
+    // table names and column names here for userdetails
     public static final String TABLE_USER_DETAILS = "UserDetails"; //table name
     public static final String USERDETAILS_USERID = "UserId";
     public static final String USERDETAILS_USERNAME = "UserName"; // one of the column of UserDetails
     public static final String USERDETAILS_FULLNAME = "FullName";// one of the column of UserDetails
     public static final String USERDETAILS_EMAILADDRESS = "EmailAddress";// one of the column of UserDetails
     public static final String USERDETAILS_PASSWORD = "Passwords";// one of the column of UserDetails
+
+    //tables and names for Property
+    public static final String TABLE_PROPERTY = "Properties";
+    public static final String PROPERTY_ID = "PropertyId";
+    public static final String PROPERTY_NAME = "PropertyName";
 
     //string with sql Query to create table
     private static final String create_table_UserDetails = "CREATE TABLE " +
@@ -24,6 +33,12 @@ public class DbHelper extends SQLiteOpenHelper {
             USERDETAILS_USERNAME + " TEXT, " +
             USERDETAILS_EMAILADDRESS + " TEXT, " +
             USERDETAILS_PASSWORD + " TEXT);";
+
+    private static final String create_table_properties = "CREATE TABLE " +
+            TABLE_PROPERTY + " (" +
+            PROPERTY_ID + " INTEGER PRIMARY KEY AUTOINCREMENT, " +
+            PROPERTY_NAME + " TEXT);";
+
     public DbHelper(Context context) {
         super(context, DATABASE_NAME, null, DATABASE_VERSION);
     }
@@ -32,10 +47,25 @@ public class DbHelper extends SQLiteOpenHelper {
     public void onCreate(SQLiteDatabase db) {
         // Create the table
         db.execSQL(create_table_UserDetails);
+        db.execSQL(create_table_properties);
     }
 
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
         // Handle database upgrades if needed
+    }
+    // Method to load buttons from the Properties table
+    public Cursor loadButtons() {
+        SQLiteDatabase db = this.getReadableDatabase();
+        String[] projection = {PROPERTY_ID, PROPERTY_NAME};
+        return db.query(TABLE_PROPERTY, projection, null, null, null, null, null);
+    }
+
+    // Method to save a button to the Properties table
+    public long saveButtonToDatabase(String buttonName) {
+        SQLiteDatabase db = this.getWritableDatabase();
+        ContentValues values = new ContentValues();
+        values.put(PROPERTY_NAME, buttonName);
+        return db.insert(TABLE_PROPERTY, null, values);
     }
 }
--- app/src/main/java/com/example/tenantbook/property_activity_2.java
+++ app/src/main/java/com/example/tenantbook/property_activity_2.java
@@ -1,15 +1,26 @@
 package com.example.tenantbook;
 
+import android.content.ContentValues;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 import android.widget.Toast;
+import android.widget.Button;
+import android.widget.EditText;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.widget.Toolbar;
 
 public class property_activity_2 extends AppCompatActivity {
+
+    private LinearLayout linearLayoutContent;
+    private DbHelper dbHelper;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,28 +30,87 @@ public class property_activity_2 extends AppCompatActivity {
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+        linearLayoutContent = findViewById(R.id.linearLayoutContent);
+        dbHelper = new DbHelper(this);
+
+        // Load existing buttons from the database
+        loadButtons();
 
     }
 
     @Override
-    public boolean onCreateOptionsMenu(Menu menu){
-        getMenuInflater().inflate(R.menu.toolbar_menu,menu);
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.toolbar_menu, menu);
         return true;
     }
 
     @Override
-    public boolean onOptionsItemSelected(@NonNull MenuItem item){
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
         int id = item.getItemId();
-        if (id == R.id.add_in_menu){
-            Toast.makeText(this,"You Clicked Add Button", Toast.LENGTH_SHORT).show();
+        if (id == R.id.add_in_menu) {
+            Toast.makeText(this, "You Clicked Add Button", Toast.LENGTH_SHORT).show();
+            showPopupWindow();
+
         }
 
-        if (id == R.id.delete_in_menu){
-            Toast.makeText(this,"You Clicked Delete Button", Toast.LENGTH_SHORT).show();
+        if (id == R.id.delete_in_menu) {
+            Toast.makeText(this, "You Clicked Delete Button", Toast.LENGTH_SHORT).show();
         }
 
         return true;
+    }
+
+    private void showPopupWindow() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("Enter Property Name");
+
+        final EditText input = new EditText(this);
+        builder.setView(input);
+
+        builder.setPositiveButton("OK", (dialog, which) -> {
+            // Create a new Button with the entered text
+            String buttonText = input.getText().toString();
+            addButton(buttonText);
+            saveButtonToDatabase(buttonText);
+        });
+
+        builder.setNegativeButton("Cancel", (dialog, which) -> dialog.cancel());
+
+        builder.show();
+    }
+
+    private void addButton(String buttonText) {
+        // Create a new Button with the entered text
+        Button button = new Button(this);
+        button.setText(buttonText);
+
+        // Add an onClickListener to the button if needed
+        button.setOnClickListener(view -> {
+            // Handle button click event
+            Toast.makeText(this, "Button Clicked: " + buttonText, Toast.LENGTH_SHORT).show();
+        });
+
+        // Add the new Button to the LinearLayout
+        linearLayoutContent.addView(button);
+    }
+
+    private void loadButtons() {
+        Cursor cursor = dbHelper.loadButtons();
+        while (cursor.moveToNext()) {
+            String buttonName = cursor.getString(cursor.getColumnIndex(DbHelper.PROPERTY_NAME));
+            addButton(buttonName);
+        }
+        cursor.close();
+    }
+
+    private void saveButtonToDatabase(String buttonName) {
+        long result = dbHelper.saveButtonToDatabase(buttonName);
+        if (result != -1) {
+            // Button saved successfully
+            Toast.makeText(this, "Button saved to database", Toast.LENGTH_SHORT).show();
+        } else {
+            // Error saving button
+            Toast.makeText(this, "Error saving button", Toast.LENGTH_SHORT).show();
+        }
     }
 }
-
-
